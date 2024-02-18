bb4ac587c8fbd2ce10300dca0f1bf4f72ee988cc
codrinursachi <codrinursachi@gmail.com>
2024-02-18 08:35:43 +0200

    Formatting

--- Lab1Ex7WON7/Program.cs
+++ Lab1Ex7WON7/Program.cs
@@ -1,7 +1,11 @@
-﻿/*Ex 7 
-Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
-daca x este divizibil cu y. 
- In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar, 
+﻿/*Ex 7 
+
+Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
+
+daca x este divizibil cu y. 
+
+ In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar, 
+
 “indivizibil”.
 */
 CheckDivisible(ReadNumbers());
